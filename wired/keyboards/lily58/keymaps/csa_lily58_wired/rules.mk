12159977d6d4b343ccc07e1fd439070056df0057
Carlos Sousa <me@carlossousa.tech>
2025-02-21 23:43:59 +0100

    change thumb helper keys

--- wired/keyboards/lily58/keymaps/csa_lily58_wired/rules.mk
+++ wired/keyboards/lily58/keymaps/csa_lily58_wired/rules.mk
@@ -1,13 +1,15 @@
 LTO_ENABLE = yes            # Link Time Optimization enabled
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
+EXTRAKEY_ENABLE = yes        # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
+
+# RGB type
 RGBLED_OPTION_TYPE = underglow
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 
