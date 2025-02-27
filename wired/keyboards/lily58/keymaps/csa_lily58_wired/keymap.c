c4098481fc62d9afeb8f22e1506fc33e410a1a8e
Carlos Sousa <me@carlossousa.tech>
2025-02-27 18:39:33 +0100

    changed fn, media, gaming activation keys

--- wired/keyboards/lily58/keymaps/csa_lily58_wired/keymap.c
+++ wired/keyboards/lily58/keymaps/csa_lily58_wired/keymap.c
@@ -20,10 +20,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Z  |   U  |   I  |   O  |   P  |  +   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ´  |  #   |
- * |------+------+------+------+------+------| MO(1) |    | MO(2) |------+------+------+------+------+------|
+ * |------+------+------+------+------+------| MO(1) |    | MO(1) |------+------+------+------+------+------|
  * |LCtrl |   Y  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   -  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI | LAlt |  DEL | / Space /       \Enter \  | BSPC | GAlt | TG(3)|
+ *                   | LGUI | LAlt |  DEL | / Space /       \Enter \  | BSPC | GAlt | MO(2)|
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                               KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
             KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                               KC_Y, KC_U, KC_I, KC_O, KC_P, KC_RBRC,
             KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,                              KC_H, KC_J, KC_K, KC_L, KC_EQL, KC_BSLS,
-            KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, MO(1),      MO(2), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                            KC_LGUI, KC_LALT, KC_DEL, KC_SPC,    KC_ENT, KC_BSPC, KC_RALT, TG(3)),
+            KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, MO(1),      MO(1), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                            KC_LGUI, KC_LALT, KC_DEL, KC_SPC,    KC_ENT, KC_BSPC, KC_RALT, MO(2)),
 
 /* FUNCTIONS
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -41,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|CapsLO|PriScr|Delete| Home |PageUp|-------.    .-------| Left | Down |  Up  | Right|      |      |
- * |------+------+------+------+------+------| MO(1) |    | MO(2)|------+------+------+------+------+------|
+ * |------+------+------+------+------+------| MO(1) |    | MO(1)|------+------+------+------+------+------|
  * |LCtrl |ScrLoc| Pause|Insert| End  |PageDo|-------|    |-------|   Ü  |   Ö  |   Ä  |      |      |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI | LAlt | DEL  | / Space /       \Enter \  | BSPC | GAlt | TG(3)|
+ *                   | LGUI | LAlt | DEL  | / Space /       \Enter \  | BSPC | GAlt | MO(2)|
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -52,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NUBS,
             KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                               KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
             KC_LSFT, KC_CAPS, KC_PSCR, KC_DEL, KC_HOME, KC_PGUP,                    KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
-            KC_LCTL, KC_SCRL, KC_PAUS, KC_INS, KC_END, KC_PGDN, MO(1),             MO(2), KC_LBRC, KC_SCLN, KC_QUOT, KC_NO, KC_NO, KC_RSFT,
-                                            KC_LGUI, KC_LALT, KC_DEL, KC_SPC,    KC_ENT, KC_BSPC, KC_RALT, TG(3)),
+            KC_LCTL, KC_SCRL, KC_PAUS, KC_INS, KC_END, KC_PGDN, MO(1),             MO(1), KC_LBRC, KC_SCLN, KC_QUOT, KC_NO, KC_NO, KC_RSFT,
+                                            KC_LGUI, KC_LALT, KC_DEL, KC_SPC,    KC_ENT, KC_BSPC, KC_RALT, MO(2)),
 
 
 /* MEDIA
@@ -63,10 +63,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |VolUp |      |      |                    |      |RGBMoP|RGBHUD|RGBSAD|RGBVAD|RGBSPD|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|MePrev|MeStop|MePlay|MeNext|      |-------.    .-------|      |      |      |      |      |      |
- * |------+------+------+------+------+------| MO(1) |    | MO(2) |------+------+------+------+------+------|
+ * |------+------+------+------+------+------| MO(1) |    | MO(1) |------+------+------+------+------+------|
  * |LCtrl |      |      |VolDow|      |      |-------|    |-------|      |      |      |      |      |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI | LAlt |  DEL |  /Space /       \Enter \  | BSPC | GAlt | TG(3)|
+ *                   | LGUI | LAlt |  DEL |  /Space /       \Enter \  | BSPC | GAlt | MO(2)|
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -74,8 +74,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                       RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,
             KC_NO, KC_NO, KC_NO, KC_KB_VOLUME_UP, KC_NO, KC_NO,                             KC_NO, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,
             KC_LSFT, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT, KC_NO,                             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-            KC_LCTL, KC_NO, KC_NO, KC_KB_VOLUME_DOWN, KC_NO, KC_NO, MO(1),             MO(2), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT,
-                                            KC_LGUI, KC_LALT, KC_DEL, KC_SPC,      KC_ENT, KC_BSPC, KC_RALT, TG(3)),
+            KC_LCTL, KC_NO, KC_NO, KC_KB_VOLUME_DOWN, KC_NO, KC_NO, TG(3),             TG(3), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_RSFT,
+                                            KC_LGUI, KC_LALT, KC_DEL, KC_SPC,      KC_ENT, KC_BSPC, KC_RALT, MO(2)),
 
 
 
